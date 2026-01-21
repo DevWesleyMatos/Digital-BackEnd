bfb56bc109fb0c15e0aee99e7965836148dd5fb9
unknown <eliasneto6010@digitalcollege.internal>
2026-01-21 19:19:10 -0300

    la vai

--- app.js
+++ app.js
@@ -11,28 +11,6 @@ app.use(bodyParser.json())
 
 app.use("/api",statusRoutes)
 
-app.get("/status/:id", async (req, res) => {
-    try {
-        let id = req.params.id
-        const data = await db.query(`SELECT * FROM status WHERE id = ${id}`)
-        res.status(200).send(data.rows)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
-app.post("/status", async (req, res) => {
-    try {
-        let { name } = req.body
-        const data = await db.query(
-            "INSERT INTO status(name) VALUES ($1) RETURNING *",
-            [name]
-        );
-        res.status(201).send(data.rows)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
 
 app.put("/status/:id", async (req, res) => {
     try {   
