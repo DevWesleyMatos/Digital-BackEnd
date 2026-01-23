c01022ab8d6e86fd310ca45d9c15bd34dc70b749
Arlenmarqs <arlenmarqs@gmail.com>
2026-01-23 17:28:15 -0300

    crud tablemark

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ import express from "express"
 import dotenv from "dotenv"
 import bodyParser from 'body-parser'
 import statusRoutes from "./routes/statusRoutes.js"
+import markRoutes from "./routes/markRoutes.js";
+
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 app.use(bodyParser.json())
 
 app.use("/api",statusRoutes)
+app.use("/api/marks", markRoutes)
 
 app.listen(3300, () => {
     console.log(`Aplicação rodando na porta ${PORT}`);
